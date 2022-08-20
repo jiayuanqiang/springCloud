49669e399fb90f0d427fc41550227762a824e901
jiayuanqiang <jia_yuanqiang@163.com>
2022-08-21 00:40:26 +0800

    config-server:3344 config-client:3355

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogFilter.java
@@ -0,0 +1,30 @@
+package com.atguigu.springcloud.filter;
+
+import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class MyLogFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (null == username) {
+            log.error("非法用户。。。。");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
